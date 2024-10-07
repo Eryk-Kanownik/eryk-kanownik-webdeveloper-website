fd17bab46771ffa841359de9c9a346772e360506
Eryk-Kanownik <eryk.patryk.kanownik@gmail.com>
2024-10-07 19:28:19 +0200

    update

--- src/components/index/Projects.tsx
+++ src/components/index/Projects.tsx
@@ -22,12 +22,11 @@ const Projects = () => {
     <section
       ref={ref}
       id="projects"
-      className="py-20 h-screen snap-start bg-black text-white">
+      className="py-20  snap-start bg-black text-white">
       <div className="container mx-auto px-6">
         <h2 className="text-3xl font-bold text-center mb-8">
           Wybrane Projekty
         </h2>
-
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {[1, 2, 3].map((project, index) => {
             return (
